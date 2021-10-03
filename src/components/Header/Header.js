b59a3ec947eb223110bdf39a036f9339fa412ad3
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-10-04 00:19:40 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/contact">Contact us</NavLink>
+        </div>
+    );
+};
+
+export default Header;
